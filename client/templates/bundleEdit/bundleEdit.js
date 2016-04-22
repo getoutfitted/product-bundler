db043b2a9edda7e62e8a9ab047cd76fcc1b82246
Paul Grever <paulgrever@gmail.com>
2016-04-22 17:28:16 -0600

    added product select functionality

--- client/templates/bundleEdit/bundleEdit.js
+++ client/templates/bundleEdit/bundleEdit.js
@@ -19,13 +19,41 @@ Template.addProductsToBundle.onCreated(function () {
   this.subscribe('ProductsForBundles');
 });
 
+Template.addProductsToBundle.onRendered(function () {
+  Session.setDefault('productSelected', false);
+  Session.set('productSelected');
+});
+
 Template.addProductsToBundle.helpers({
   allTopProducts: function () {
     return ReactionCore.Collections.Products.find({
       shopId: ReactionCore.getShopId(),
       type: 'simple',
       ancestors: [],
-      funtionalType: { $ne: 'bundle'}
-    });
+      functionalType: { $ne: 'bundle'}
+    }).fetch();
+  },
+  allVariantsForSelectedProduct: function () {
+    let productId = Session.get('productToAddToBundle');
+    return ReactionCore.Collections.Products.find({
+      shopId: ReactionCore.getShopId(),
+      type: 'variant',
+      color: {$exists: true},
+      size: {$exists: true},
+      ancestors: productId,
+      functionalType: { $ne: 'bundleVariant'}
+    }).fetch();
+  },
+  productSelected: function () {
+    return Session.get('productSelected');
+  }
+});
+
+Template.addProductsToBundle.events({
+  'change #selectProduct': function (event) {
+    event.preventDefault();
+    let productId = event.target.value;
+    Session.set('productSelected', true);
+    Session.set('productToAddToBundle', productId);
   }
 });
