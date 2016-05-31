089a82663227911837c8a4e7eaa89a17343edd16
Paul Grever <paulgrever@gmail.com>
2016-05-31 16:30:24 -0600

    added before hook

--- server/hooks/before_copy_to_cart_hook.js
+++ server/hooks/before_copy_to_cart_hook.js
@@ -1,5 +1,21 @@
 ReactionCore.MethodHooks.before('cart/copyCartToOrder', function (options) {
   const cart = ReactionCore.Collections.Cart.findOne(options.arguments[0]);
-  console.log('cart', cart)
+  if (cart) {
+    _.each(cart.items, function (item) {
+      if (item.variants.functionalType === 'bundleVariant') {
+        // let additionalItems = [];
+        let quantityRange = _.range(1, item.quantity + 1);
+        _.each(quantityRange, function (itemQty) {
+          let chosen = _.find(item.variants.selectedBundleOptions, function (option) {
+            return option.selectionForQtyNumber === itemQty;
+          });
+          let chosenVariants = _.where(item.variants.selectedBundleOptions, {selectionForQtyNumber: itemQty});
+          _.each(chosenVariants, function (chosenOption) {
+            Meteor.call('cart/addToCart', chosenOption.productId, chosenOption.variantId);
+          });
+        });
+      }
+    });
+  }
   return options;
 });
