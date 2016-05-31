479ac2efae5d2877b8e20cdb1c8701fdf35e74c0
Paul Grever <paulgrever@gmail.com>
2016-05-31 14:43:20 -0600

    added method that tracks options selected in the cart

--- server/methods/cart.js
+++ server/methods/cart.js
@@ -13,67 +13,60 @@ Meteor.methods({
     check(productId, String);
     check(variantId, String);
     check(selectedVariants, [String]);
-    // let cart = ReactionCore.Collections.Cart.findOne(cartId);
-    // // console.log('cart', cart)
-    // let items = cart.items;
-    // let thisBundleItem = _.find(items, function (item) {
-    //   // const bundleType = item.variants.functionalType === 'bundleVariant';
-    //   const noSelections = item.variants.selectedBundleOptions.length <= 0;
-    //   const correctProductId  = item.productId === productId;
-    //   const correctVariantId = item.variants._id === variantId;
-    //   if (noSelections && correctProductId && correctVariantId) {
-    //     return item;
-    //   }
-    // });
-    // let selectedBundleOptions = [];
-    // _.each(thisBundleItem.bundleProducts, function (bundleProduct) {
-    //   let options = {
-    //     productId: bundleProduct.productId
-    //   }
+    const { Log } = ReactionCore;
+    const cart = ReactionCore.Collections.Cart.findOne({ userId: this.userId });
+    if (!cart) {
+      Log.error(`Cart not found for user: ${ this.userId }`);
+      throw new Meteor.Error(404, "Cart not found",
+        "Cart not found for user with such id");
+    }
 
-    // })
+    let items = cart.items;
+    let thisBundleItem = _.find(items, function (item) {
+      const correctProductId  = item.productId === productId;
+      const correctVariantId = item.variants._id === variantId;
+      const isBundle = item.variants.functionalType === 'bundleVariant';
+      if (correctProductId && correctVariantId && isBundle) {
+        return item;
+      }
+    });
+    const itemNumber = thisBundleItem.quantity;
+    const numberOfBundleOptions = thisBundleItem.variants.bundleProducts.length;
+    if (numberOfBundleOptions !== selectedVariants.length) {
+      Log.error(`Not all options were selected for item ${thisBundleItem._id} in Cart ${cart._id} `)
+    }
 
-    // let bundleItem = _.findWhere(cart.items, {
-    //   'productId': productId,
-    //   'variants._id': variantId
-    //   // 'variants.selectedBundleOption': []
-    // });
-    // console.log('bundle', bundleItem);
-    // let selectedOptions = [];
-    // let bundleItem = _.find(cart.items, function(item) {
-    //   item.functionalType
-    // })
-    // _.each(cart.items.productBundles, function (productBundle, index) {
-    //   let selectedInfo = {};
-    //   selectedInfo.productId = productBundle.productId;
-    //   if (verifyCorrectProduct(productBundle.variantIds, selectedVariants[index])) {
-    //     selectedInfo.variantId = selectedVariants[index];
+    let selectedOptions = thisBundleItem.variants.selectedBundleOptions || [];
+    _.each(thisBundleItem.variants.bundleProducts, function (bundleProduct) {
+      let bundleSelection = {
+        selectionForQtyNumber: itemNumber,
+        productId: bundleProduct.productId
+      };
+      let selectedVariant = _.find(bundleProduct.variantIds, function (option) {
+        return _.contains(selectedVariants, option.variantId);
+      });
+      bundleSelection.variantId = selectedVariant.variantId;
+      if (bundleProduct.label && selectedVariant.label) {
+        bundleSelection.cartLabel = `${bundleProduct.label} ${selectedVariant.label}`;
+      } else if (bundleProduct.label) {
+        bundleSelection.cartLabel = `${bundleProduct.label}`;
+      } else if (selectedVariant.label) {
+        bundleSelection.cartLabel = `${selectedVariant.label}`;
+      } else {
+        let label = ReactionCore.Collections.Products.findOne(selectedVariant.variantId);
+        return label.vendor + label.title;
+      }
+      selectedOptions.push(bundleSelection);
+    });
 
-    //   }
-    // });
-    // let modifiedSelectedVariants = [];
-    // _.each(selectedVariants, function (v) {
-    //   let t = {};
-    //   t.variandId = v;
-    //   modifiedSelectedVariants.push(t);
-    // });
-    // let t  = [{
-    //   variantId: selectedVariants[0]
-    // },
-    // {
-    //   variantId: selectedVariants[1]
-    // }]
-    // ReactionCore.Collections.Cart.update({
-    //   '_id': cartId,
-    //   'items.productId': productId,
-    //   'items.variants._id': variantId,
-    //   'items.vartiants.selectedBundleOptions': {
-    //     $exists: false
-    //   }
-    // }, {
-    //   $set: {
-    //     'items.$.variants.selectedBundleOptions': t
-    //   }
-    // });
+    ReactionCore.Collections.Cart.update({
+      '_id': cart._id,
+      'items.productId': productId,
+      'items.variants._id': variantId
+    }, {
+      $set: {
+        'items.$.variants.selectedBundleOptions': selectedOptions
+      }
+    });
   }
 });
